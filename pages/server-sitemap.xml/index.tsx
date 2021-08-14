4c367bd01fd2ea50ca043a85509728472fbac606
Gavin Grant <gavingrant@gmail.com>
2021-08-13 18:40:34 -0700

    feat: add home and products pages to sitemap

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -1,17 +1,22 @@
 import { GetServerSideProps } from "next";
 import { getServerSideSitemap, ISitemapField } from "next-sitemap";
 
-const fetcher = url => fetch(url).then(r => r.json())
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const products = await fetcher('https://www.omitplastic.com/api/products')
+  const products = await fetcher("https://www.omitplastic.com/api/products");
 
-  const fields: ISitemapField[] = products.map((product) => ({
-    loc: `https://www.omitplastic.com/product/${product.slug}`,
-    lastmod: new Date().toISOString(),
-  }));
-  
+  const fields: ISitemapField[] = products
+    .map((product) => ({
+      loc: `https://www.omitplastic.com/product/${product.slug}`,
+      lastmod: new Date().toISOString(),
+    }))
+    .concat([
+      { loc: "https://www.omitplastic.com", lastmod: new Date().toISOString() },
+      { loc: "https://www.omitplastic.com/products", lastmod: new Date().toISOString() },
+    ]);
+
   return getServerSideSitemap(ctx, fields);
-}
+};
 
 export default function Site() {}
