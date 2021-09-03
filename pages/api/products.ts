4569c425a62a3afd536bd19efda2b4fd828eed4e
Gavin Grant <gavingrant@gmail.com>
2021-09-02 22:42:28 -0700

    refactor: revise sort order of products

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -2,7 +2,17 @@ import prisma from "../../lib/prisma";
 
 export default async function handle(req, res) {
   const products = await prisma.product.findMany({
-    orderBy: [{ name: "asc" }],
+    orderBy: [
+      {
+        category: 'desc',
+      },
+      {
+        type: 'asc',
+      },
+      {
+        name: 'asc',
+      },
+    ],
   });
   res.json(products);
 }
