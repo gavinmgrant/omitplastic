14beaa3817f99a4e13c10bfc2b7317b79743558a
Gavin Grant <gavingrant@gmail.com>
2021-09-18 16:03:36 -0700

    chore: link in product card title

--- components/Product.tsx
+++ components/Product.tsx
@@ -23,25 +23,25 @@ const Product: React.FC<{ product: ProductProps }> = ({ product }) => {
   const [productName, setProductName] = useState(product.name);
 
   useEffect(() => {
-    if (product.name.length > 90) {
-      setProductName(product.name.substring(0, 90) + "...");
+    if (product.name.length > 107) {
+      setProductName(product.name.substring(0, 107) + "...");
     }
   }, [product.name]);
 
   return (
     <div className="h-60 lg:h-64 flex flex-col justify-between">
-      <Link href={`/product/${product.slug}`}>
-        <a className="flex justify-between cursor-pointer">
-          <div className="pt-2 pr-6 w-2/5 md:pr-10">
-            <img
-              src={product.imageUrl}
-              alt={product.name}
-              className="max-h-44"
-            />
-          </div>
-          <h2 className="w-3/5 font-serif text-lg text-black">{productName}</h2>
-        </a>
-      </Link>
+      <div className="flex justify-between cursor-pointer">
+        <div className="pt-2 pr-6 w-2/5 md:pr-10">
+          <img src={product.imageUrl} alt={product.name} className="max-h-44" />
+        </div>
+        <div className="w-3/5">
+          <Link href={`/product/${product.slug}`}>
+            <a>
+              <h2 className="font-serif leading-snug text-lg text-black">{productName}</h2>
+            </a>
+          </Link>
+        </div>
+      </div>
 
       <div className="flex justify-between">
         <ul className="flex">
