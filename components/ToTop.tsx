b73d4094e266353581bd890ee806a79884b07618
Gavin Grant <gavingrant@gmail.com>
2021-09-20 18:42:10 -0700

    feat: hide to top button when at top of page

--- components/ToTop.tsx
+++ components/ToTop.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IconChevronUp } from "@tabler/icons";
 
 const ToTop: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const handleScroll = () => {
+    const top = window.scrollY;
+    setIsVisible(top > 100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <button
-      onClick={scrollToTop}
-      className="bg-white fixed bottom-4 right-4 md:right-8 transition-all duration-500 p-3 border-black shadow-lg rounded-full border-2 border-solid z-40"
-    >
-      <IconChevronUp />
-    </button>
+    isVisible && (
+      <button
+        onClick={scrollToTop}
+        className="bg-white fixed bottom-4 right-4 md:right-8 transition-all duration-500 p-3 border-black shadow-lg rounded-full border-2 border-solid z-40"
+      >
+        <IconChevronUp />
+      </button>
+    )
   );
 };
 
