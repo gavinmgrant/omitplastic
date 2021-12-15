5d385908ab4b4d632e4c16e1f345da8d5b2a9364
Gavin Grant <gavingrant@gmail.com>
2021-12-14 22:12:30 -0800

    feat: add sitemap link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,30 +3,35 @@ import Link from "next/link";
 import { IconHeart } from "@tabler/icons";
 
 const Footer: React.FC = () => {
-
   return (
-    <div className="flex flex-col text-center items-center justify-between px-4 py-12 md:px-8 md:py-16 md:text-left md:flex-row bg-custom-green text-white">
-      <div className="flex flex-col order-last pt-8 md:pt-0">
-        <p className="pb-2">
-          As an Amazon Associate we earn from qualifying purchases.
-        </p>
-        <p className="pb-2 text-white">
-          Made with <IconHeart size={19} className="inline-block mb-1" /> in San Diego.
-        </p>
-        <span className="pb-2 text-white">© 2021 OmitPlastic</span>
-      </div>
-      <div className="flex flex-col text-center md:text-right md:order-last">
-        <Link href="/faq">
-          <a className="pb-2 text-white">Frequently Asked Questions</a>
-        </Link>
-        <Link href="/terms">
-          <a className="pb-2 text-white">Terms and Conditions</a>
-        </Link>
-        <Link href="/privacy">
-          <a className="pb-2 text-white">Privacy Policy</a>
-        </Link>
-      </div>
-    </div>
+    <footer className="bg-custom-green text-white text-center pb-3">
+      <section className="flex flex-col items-center justify-between px-4 py-8 md:px-8 md:py-12 md:text-left md:flex-row ">
+        <div className="flex flex-col order-last pt-8 md:pt-0">
+          <p className="pb-2">
+            As an Amazon Associate we earn from qualifying purchases.
+          </p>
+          <p className="pb-2 text-white">
+            Made with <IconHeart size={19} className="inline-block mb-1" /> in
+            San Diego.
+          </p>
+          <Link href="/server-sitemap.xml">
+            <a className="pb-2 text-white">Sitemap</a>
+          </Link>
+        </div>
+        <div className="flex flex-col text-center md:text-right md:order-last">
+          <Link href="/faq">
+            <a className="pb-2 text-white">Frequently Asked Questions</a>
+          </Link>
+          <Link href="/terms">
+            <a className="pb-2 text-white">Terms and Conditions</a>
+          </Link>
+          <Link href="/privacy">
+            <a className="pb-2 text-white">Privacy Policy</a>
+          </Link>
+        </div>
+      </section>
+      <p className="pb-2 text-white">© 2021 OmitPlastic</p>
+    </footer>
   );
 };
 
